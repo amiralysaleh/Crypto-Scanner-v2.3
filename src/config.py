c8303ad81845f5185dbfadaeb896a0d8ba51fdbc
Amirali <126765964+amiralysaleh@users.noreply.github.com>
2025-06-09 06:27:36 +0330

    Update config.py

--- src/config.py
+++ src/config.py
@@ -89,7 +89,7 @@ SCALPING_SETTINGS = {
 # تنظیمات تایم فریم‌ها
 PRIMARY_TIMEFRAME = "1hour"
 HIGHER_TIMEFRAME = "4hour"
-KLINE_SIZE = 500
+KLINE_SIZE = 900
 SIGNALS_FILE = "data/signals.json"
 
 # تنظیمات API کوکوین
